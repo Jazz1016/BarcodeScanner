cbc8a7c513e9bf1d91ec4be412fb9903d1b4fcf9
James Lea <james.q.lea@gmail.com>
2023-05-14 19:20:22 -0600

    completed camera errors, started on coordinator

---
+++ BarcodeScanner/ScannerView.swift
@@ -0,0 +1,41 @@
+//
+//  ScannerView.swift
+//  BarcodeScanner
+//
+//  Created by James Lea on 5/14/23.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        func didFind(barcode: String) {
+            print(barcode)
+        }
+        
+        func didSurface(error: CameraError) {
+            print(error.rawValue)
+        }
+    }
+    
+    
+}
+
+
+struct ScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScannerView()
+    }
+}
